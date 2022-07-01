8c7ef11bbde0e8e4e921e263e34c90e2e7ca3f81
Omar Ramos <omar@webdecero.com>
2022-07-01 00:19:30 -0500

    create panel view

--- src/plugins/vee-validate.js
+++ src/plugins/vee-validate.js
@@ -3,10 +3,6 @@ import { extend, ValidationProvider, ValidationObserver } from 'vee-validate'
 import * as rules from 'vee-validate/dist/rules'
 import { messages } from 'vee-validate/dist/locale/es.json'
 
-import {
-  getValidateCollectionTemplate
-} from '@/api/collectionTemplate'
-
 Object.keys(rules).forEach(rule => {
   extend(rule, {
     ...rules[rule], // copies rule configuration
@@ -15,33 +11,6 @@ Object.keys(rules).forEach(rule => {
 })
 Vue.component('ValidationProvider', ValidationProvider)
 Vue.component('ValidationObserver', ValidationObserver)
-
-extend('key', {
-  validate: async (value, args) => {
-    console.log(args)
-    const params = {
-      key: value
-    }
-    if (args._id !== undefined) { params._id = args._id }
-
-    const valid = await getValidateCollectionTemplate(params).then(response => {
-      return response.data
-    })
-    return valid
-  },
-  message: 'This key Exist',
-  params: ['_id']
-})
-
-extend('https', {
-  validate: value => {
-    // eslint-disable-next-line prefer-regex-literals
-    const regexp = new RegExp(/^(https:\/\/)([\da-z.-]+\.[a-z.]{2,6}|[\d.]+)([/:?=&#]{1}[\da-z.-]+)*[/?]?$/, 'i')
-
-    return regexp.test(value)
-  },
-  message: 'The {_field_} field must be a valid URL secure'
-})
 
 extend('http', {
   validate: value => {
--- src/views/panel/PanelRestaurants.vue
+++ src/views/panel/PanelRestaurants.vue
@@ -1,15 +1,127 @@
 <template>
   <div>
-    <el-row>
-      <el-col>
-        <el-button type="primary" plain @click="goToCreateItem"
-          >Primary</el-button
-        >
+    <el-card class="navbar" shadow="hover">
+      <el-row >
+        <el-col :span="2">
+          <img src="../../assets/img/restaurant-icon.jpg" alt="restaurant" class="image">
+        </el-col>
+        <el-col :span="6" >
+          <div class="title-card">
+            <b>Restaurants :D</b>
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="user-card">
+            Welcome: {{ userCard }}
+          </div>
+        </el-col>
+        <el-col :span="9">
+          <el-button type="info" class="button-home" plain><b>Logout</b></el-button>
+          <el-button
+            type="success"
+            style="margin-top: 1rem;"
+            @click="handleRegister"
+            plain><b>Register a new Restaurant</b></el-button>
+          <!-- <el-button type="success" class="button-home" plain>Login</el-button> -->
+        </el-col>
+      </el-row>
+    </el-card>
+    <el-row style="margin-top: 2rem;" type="flex">
+      <el-col :span="activeForm ? 14 : 24">
+        <el-card style="margin-left: 1rem; margin-right: 1rem;">
+          <el-table
+            :data="tableData"
+            stripe
+            style="width: 100%">
+            <el-table-column
+              prop="name"
+              label="Name resturant"
+              :width="activeForm ? 180 : false">
+            </el-table-column>
+            <el-table-column
+              prop="type"
+              label="Type food"
+              :width="activeForm ? 100 : false">
+            </el-table-column>
+            <el-table-column
+              prop="address"
+              label="Resturant address"
+              :width="activeForm ? 300 : false">
+            </el-table-column>
+            <el-table-column
+              prop="telephone"
+              label="Resturant telephone"
+              :width="activeForm ? 180 : false">
+            </el-table-column>
+            <el-table-column
+              fixed="right"
+              label="Operaciones"
+              width="180">
+              <template slot-scope="scope">
+                <el-button
+                  @click.native.prevent="handleEdit(scope.$index, tableData)"
+                  type="warning"
+                  size="mini">Edit</el-button>
+                <el-button
+                  @click.native.prevent="handleDelete(scope.$index, tableData)"
+                  type="danger"
+                  size="mini">Delete</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-card>
       </el-col>
-      <el-col>
-        <el-button type="success" plain @click="goToPuchaseItem"
-          >Success</el-button
-        >
+      <el-col :span="activeForm ? 10 : 0">
+        <el-card shadow="hover" >
+          <div slot="header" class="clearfix" style="align: center">
+            <span>{{ titleCard }}</span>
+          </div>
+          <el-form
+          :label-position="labelPosition"
+          label-width="100px"
+          :model="formLabelAlign">
+          <el-form-item
+            label="Restaurant name">
+            <el-input v-model="formLabelAlign.name"></el-input>
+          </el-form-item>
+          <el-form-item
+            label="Kind of food">
+            <el-input v-model="formLabelAlign.type"></el-input>
+          </el-form-item>
+          <el-form-item
+            label="Restaurant address">
+            <el-input v-model="formLabelAlign.address"></el-input>
+          </el-form-item>
+          <el-form-item
+            label="Restaurant telephone">
+            <el-input v-model="formLabelAlign.telephone"></el-input>
+          </el-form-item>
+        </el-form>
+        <div class="bottom clearfix button-form">
+          <el-row>
+            <el-col :span="5">
+              <el-button
+                type="danger"
+                class="button"
+                @click="() => (this.activeForm = false)"
+                icon="el-icon-back">Cancel</el-button>
+            </el-col>
+            <el-col :span="5" v-if="isEdit">
+              <el-button
+                type="warning"
+                @click="handleEdit"
+                class="button"
+                icon="el-icon-collection-tag">Save</el-button>
+            </el-col>
+            <el-con :span="5" v-else>
+              <el-button
+                type="success"
+                class="button"
+                icon="el-icon-plus">Register</el-button>
+            </el-con>
+          </el-row>
+        </div>
+        </el-card>
       </el-col>
     </el-row>
   </div>
@@ -19,17 +131,98 @@
 export default {
   data () {
     return {
-      loading: false
+      activeForm: false,
+      isEdit: true,
+      titleCard: '',
+      userCard: 'Omar Ramos',
+      tableData: [{
+        name: '2016-05-03',
+        type: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+        telephone: '555555555'
+      }, {
+        name: '2016-05-03',
+        type: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+        telephone: '555555555'
+      }, {
+        name: '2016-05-03',
+        type: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+        telephone: '555555555'
+      }, {
+        name: '2016-05-03',
+        type: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+        telephone: '555555555'
+      }],
+      labelPosition: 'top',
+      formLabelAlign: {
+        name: '',
+        region: '',
+        type: ''
+      }
     }
   },
-  created () {},
   methods: {
-    goToCreateItem () {
-      this.$router.push({ name: 'create.item' })
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath)
     },
-    goToPuchaseItem () {
-      this.$router.push({ name: 'create.item' })
+    handleRegister () {
+      this.titleCard = 'Register a new Restaurant'
+      this.isEdit = false
+      this.activeForm = true
+    },
+    handleEdit () {
+      this.titleCard = 'Edit a new Restaurant'
+      this.activeForm = true
+      this.isEdit = true
+    },
+    handleDelete (index, rows) {
+      this.$confirm('This will permanently delete the restaurant. Continue?', 'Warning', {
+        confirmButtonText: 'OK',
+        cancelButtonText: 'Cancel',
+        type: 'warning'
+      }).then(() => {
+        this.tableData.splice(index, 1)
+        this.$message({
+          type: 'success',
+          message: 'Delete completed'
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: 'Delete canceled'
+        })
+      })
     }
   }
 }
 </script>
+
+<style scoped>
+.button-home {
+  float: right;
+  margin-right: 40px;
+  margin-top: 1rem;
+  width: 100px;
+}
+.title-card {
+  margin-left: 12px;
+  font-family:'Courier New', Courier, monospace;
+  margin-top: 1rem;
+  font-size: 2rem;
+}
+.user-card {
+  margin-top: 1.5rem;
+  font-size: 20px;
+  color: #7D9D9C;
+}
+.navbar {
+  background-color: #f5f3f0;
+}
+.image {
+  width: 70%;
+  display: block;
+}
+</style>
